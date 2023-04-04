dba1109cdf1b4096495523c91ce85ad9672df54b
habeeb <habeebalao101@gmail.com>
2023-04-05 00:02:45 +0100

    fixes

--- ASSETS/JS/luxodrive/script.js
+++ ASSETS/JS/luxodrive/script.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
 
+    $('.nav-menu a').on('click', function(event) {
+        // Prevent default link behavior
+        event.preventDefault();
+
+        // Get the target element's ID from the href attribute
+        var targetId = $(this).attr('href');
+
+        // Use the target ID to get the target element
+        var $targetElement = $(targetId);
+
+        // Smoothly scroll to the target element
+        $('html, body').animate({
+            scrollTop: $targetElement.offset().top
+        }, 1000);
+    });
 
 });
