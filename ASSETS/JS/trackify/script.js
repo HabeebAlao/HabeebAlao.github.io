bc9b22451a2131f0bf3a9f208694579f0d3165e0
habeeb <habeebalao101@gmail.com>
2023-04-04 17:38:07 +0100

    updated file strucure and implemented nav menu and landing section for trackify

---
+++ ASSETS/JS/trackify/script.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+    // Add click event listener to all menu links
+    $('.nav-menu a').on('click', function(event) {
+        // Prevent default link behavior
+        event.preventDefault();
+
+        // Get the target element's ID from the href attribute
+        var targetId = $(this).attr('href');
+
+        // Use the target ID to get the target element
+        var $targetElement = $(targetId);
+
+        // Smoothly scroll to the target element
+        $('html, body').animate({
+            scrollTop: $targetElement.offset().top
+        }, 1000);
+    });
+});
