cb614b86219f2c8d223f21a73770078d68e7356a
habeeb <habeebalao101@gmail.com>
2023-04-09 22:23:32 +0100

    updates for luxodrive

--- ASSETS/JS/index/script.js
+++ ASSETS/JS/index/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-    // Show the button when the user has scrolled past 500 pixels
     $(window).scroll(function() {
         if ($(this).scrollTop() > 500) {
             $('#back-to-top-btn').fadeIn();
@@ -17,7 +16,7 @@ $(document).ready(function() {
 
     $('#about > div > div > div > ul > li').on('click', function() {
         console.log("hello");
-        // Remove the active class from any li elements that have it
+
         var curr = $('#about > div > div > div > ul > li.active').data("name");
         $('#about > div > div > div > ul > li.active').removeClass('active');
 
@@ -26,23 +25,20 @@ $(document).ready(function() {
         var next = $(this).data('name');
         $('#' + next).show();
 
-        // Add the active class to the hovered li element
+
         $(this).addClass('active');
     });
 
     $(document).ready(function() {
-        // Add click event listener to all menu links
+
         $('.nav-menu a').on('click', function(event) {
-            // Prevent default link behavior
+
             event.preventDefault();
 
-            // Get the target element's ID from the href attribute
             var targetId = $(this).attr('href');
 
-            // Use the target ID to get the target element
             var $targetElement = $(targetId);
 
-            // Smoothly scroll to the target element
             $('html, body').animate({
                 scrollTop: $targetElement.offset().top
             }, 1000);
@@ -51,20 +47,18 @@ $(document).ready(function() {
     $(document).ready(function() {
         var $iframe = $('#iframe-container iframe');
     
-        // Smooth scrolling when hovering over the iframe
         $iframe.on('mouseover', function() {
             var iframeDoc = this.contentWindow.document;
             $('html, body', iframeDoc).stop().animate({
                 scrollTop: $(iframeDoc).height()
-            }, 2000);
+            }, 4000);
         });
     
-        // Smooth scrolling back to the top when not hovering
         $iframe.on('mouseout', function() {
             var iframeDoc = this.contentWindow.document;
             $('html, body', iframeDoc).stop().animate({
                 scrollTop: 0
-            }, 2000);
+            }, 4000);
         });
     });
     
--- ASSETS/JS/luxodrive/script.js
+++ ASSETS/JS/luxodrive/script.js
@@ -1,19 +1,95 @@
 $(document).ready(function() {
 
     $('.nav-menu a').on('click', function(event) {
-        // Prevent default link behavior
+
         event.preventDefault();
 
-        // Get the target element's ID from the href attribute
+
         var targetId = $(this).attr('href');
 
-        // Use the target ID to get the target element
+
         var $targetElement = $(targetId);
 
-        // Smoothly scroll to the target element
+
         $('html, body').animate({
             scrollTop: $targetElement.offset().top
         }, 1000);
     });
 
+const carDetails = {
+    "cullinan": {
+        "make": "Rolls Royce",
+        "year": "2020",
+        "fuel": "Petrol",
+        "litre": "6.7",
+        "imageUrl": "url_to_cullinan_image.jpg"
+    },
+    "g class": {
+        "make": "Mercedes",
+        "year": "2021",
+        "fuel": "Petrol",
+        "litre": "4.0",
+        "imageUrl": "/HabeebAlao.github.io/ASSETS/images/gClass2.png"
+    },
+    "wraith": {
+        "make": "Rolls Royce",
+        "year": "2022",
+        "fuel": "Petrol",
+        "litre": "6.6",
+        "imageUrl": "url_to_wraith_image.jpg"
+    },
+    "wrangler": {
+        "make": "Jeep",
+        "year": "2021",
+        "fuel": "Petrol",
+        "litre": "3.6",
+        "imageUrl": "url_to_wrangler_image.jpg"
+    },
+    "hurrican": {
+        "make": "Lamborghini",
+        "year": "2021",
+        "fuel": "Petrol",
+        "litre": "5.2",
+        "imageUrl": "url_to_hurrican_image.jpg"
+    },
+    "812": {
+        "make": "Ferrari",
+        "year": "2021",
+        "fuel": "Petrol",
+        "litre": "6.5",
+        "imageUrl": "url_to_812_image.jpg"
+    }
+};
+
+
+$(".model-selector button").on("click", function() {
+
+
+    $(".model-selector button.active").removeClass("active");
+    
+
+    $(this).addClass("active");
+
+    const carName = $(this).text().toLowerCase();
+        
+
+    $("#make").text(carDetails[carName].make);
+    $("#year p").text(carDetails[carName].year);
+    $("#fuel p").text(carDetails[carName].fuel);
+    $("#litre p").text(carDetails[carName].litre);
+
+
+
+    $(".car-display").css("background-image", "url(" + carDetails[carName].imageUrl + ")");
+});
+
+$('.question').on('click', function () {
+    $('#faqs > div > div.question.active').removeClass('active');
+    $('.answer').hide();
+
+    $(this).addClass('active');
+    console.log(this);
+    $($(this).next()).slideDown( "slow");
+    
+})
 });
