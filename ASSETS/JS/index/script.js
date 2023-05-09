335af645a842ce052853106786114ddffa110a23
habeeb <habeebalao101@gmail.com>
2023-05-10 00:38:47 +0100

    updates and mobile parallax effect

--- ASSETS/JS/index/script.js
+++ ASSETS/JS/index/script.js
@@ -15,16 +15,26 @@ $(document).ready(function() {
     });
 
     $('#about > div > div > div > ul > li').on('click', function() {
-        console.log("hello");
 
+        //get whats cufrently active
         var curr = $('#about > div > div > div > ul > li.active').data("name");
-        $('#about > div > div > div > ul > li.active').removeClass('active');
+        var curr_header = $('#about > div > div h4.active-header').data("name");
 
+        //remove active and hide
+        $('#about > div > div > div > ul > li.active').removeClass('active');
         $('#' + curr).hide();
 
+        // hide and remove active
+        $('#about > div > div h4.active-header').hide();
+        $('#about > div > div h4.active-header').removeClass('active-header');
+
+        
+        // get what was just clicked and set to show
         var next = $(this).data('name');
         $('#' + next).show();
-
+        console.log(next);
+        $('#about > div > div h4[data-name="' + next + '"]').show();
+        $('#about > div > div h4[data-name="' + next + '"]').addClass('active-header');
 
         $(this).addClass('active');
     });
@@ -61,5 +71,45 @@ $(document).ready(function() {
             }, 4000);
         });
     });
+
+    if ($(window).width() <= 600) {
+        $(window).scroll(function() {
+          var scrollTop = $(this).scrollTop();
+      
+          // Parallax effect for the image
+          $('.landing-img-holder img').css({
+            'transform': 'translateY(' + scrollTop / 6 + 'px)'+'translateX(-2.4000000000000004vw)',
+            'overflow-x': 'hidden'
+          });
+      
+          // Parallax effect for the landing-info text
+          $('#landing-info').css({
+            'font-size': 30 + scrollTop / 20 + 'px',
+            'overflow-x': 'hidden',
+            'white-space': 'nowrap',
+            'text-align': 'centre'
+          });
+      
+          // Parallax effect for the landing-name
+          $('#landing-name').css({
+            'animation': 'landing-name 1s forwards',
+            'animation-play-state': 'paused',
+            'transform': 'translateY(' + scrollTop / 1.3 + 'px)',
+            'font-size': 55 + scrollTop / 10 + 'px',
+            'max-width': '100%',
+            'overflow-x': 'hidden'
+            
+          });
+      
+          if (scrollTop > 100) {
+            $('#landing-name').css({
+              'animation-play-state': 'running',
+              'overflow-x': 'hidden'
+            });
+          }
+        });
+      }
+      
+      
     
 });
